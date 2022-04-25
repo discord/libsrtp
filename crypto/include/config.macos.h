aebbad8a2e0cbf363713545b20d7799ec3bc3c07
anurag <anurag.mishra@discordapp.com>
2022-04-25 14:54:39 -0700

    Updated config.*.h files to build discord_rtc_worker with latest libsrtp

--- crypto/include/config.macos.h
+++ crypto/include/config.macos.h
@@ -1,26 +1,26 @@
 /* crypto/include/config.h.  Generated from config_in.h by configure.  */
-/* config_in.h.  Generated from configure.in by autoheader.  */
+/* config_in.h.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Define if building for a CISC machine (e.g. Intel). */
-#define CPU_CISC 1
+/* #undef CPU_CISC */
 
 /* Define if building for a RISC machine (assume slow byte access). */
-/* #undef CPU_RISC */
+#define CPU_RISC 1
 
-/* Path to random device */
-#define DEV_URANDOM "/dev/urandom"
+/* Define to enabled debug logging for all mudules. */
+/* #undef ENABLE_DEBUG_LOGGING */
 
-/* Define to compile in dynamic debugging system. */
-#define ENABLE_DEBUGGING 1
-
-/* Report errors to this file. */
+/* Logging statments will be writen to this file. */
 /* #undef ERR_REPORTING_FILE */
 
-/* Define to use logging to stdout. */
-#define ERR_REPORTING_STDOUT 1
+/* Define to redirect logging to stdout. */
+/* #undef ERR_REPORTING_STDOUT */
 
-/* Define this to use ISMAcryp code. */
-/* #undef GENERIC_AESICM */
+/* Define this to use AES-GCM. */
+/* #undef GCM */
 
 /* Define to 1 if you have the <arpa/inet.h> header file. */
 #define HAVE_ARPA_INET_H 1
@@ -30,6 +30,9 @@
 
 /* Define to 1 if you have the `inet_aton' function. */
 #define HAVE_INET_ATON 1
+
+/* Define to 1 if you have the `inet_pton' function. */
+#define HAVE_INET_PTON 1
 
 /* Define to 1 if the system has the type `int16_t'. */
 #define HAVE_INT16_T 1
@@ -43,8 +46,20 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the `dl' library (-ldl). */
+/* #undef HAVE_LIBDL */
+
+/* Define to 1 if you have the `nspr4' library (-lnspr4). */
+/* #undef HAVE_LIBNSPR4 */
+
+/* Define to 1 if you have the `nss3' library (-lnss3). */
+/* #undef HAVE_LIBNSS3 */
+
 /* Define to 1 if you have the `socket' library (-lsocket). */
 /* #undef HAVE_LIBSOCKET */
+
+/* Define to 1 if you have the `z' library (-lz). */
+/* #undef HAVE_LIBZ */
 
 /* Define to 1 if you have the <machine/types.h> header file. */
 #define HAVE_MACHINE_TYPES_H 1
@@ -54,6 +69,12 @@
 
 /* Define to 1 if you have the <netinet/in.h> header file. */
 #define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <nss.h> header file. */
+/* #undef HAVE_NSS_H */
+
+/* Define to 1 if you have the `winpcap' library (-lwpcap) */
+#define HAVE_PCAP 1
 
 /* Define to 1 if you have the `sigaction' function. */
 #define HAVE_SIGACTION 1
@@ -72,9 +93,6 @@
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <syslog.h> header file. */
-#define HAVE_SYSLOG_H 1
 
 /* Define to 1 if you have the <sys/int_types.h> header file. */
 /* #undef HAVE_SYS_INT_TYPES_H */
@@ -118,51 +136,56 @@
 /* Define to use X86 inlined assembly code */
 /* #undef HAVE_X86 */
 
+/* Define this to use NSS crypto. */
+/* #undef NSS */
+
+/* Define this to use OpenSSL crypto. */
+/* #undef OPENSSL */
+
+/* Define this if OPENSSL_cleanse is broken. */
+/* #undef OPENSSL_CLEANSE_BROKEN */
+
+/* Define this to use OpenSSL KDF for SRTP. */
+/* #undef OPENSSL_KDF */
+
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "https://github.com/cisco/libsrtp/issues"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "libsrtp"
+#define PACKAGE_NAME "libsrtp2"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libsrtp 1.5.2"
+#define PACKAGE_STRING "libsrtp2 2.5.0-pre"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libsrtp"
+#define PACKAGE_TARNAME "libsrtp2"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.5.2"
+#define PACKAGE_VERSION "2.5.0-pre"
 
-/* The size of a `unsigned long', as computed by sizeof. */
+/* The size of `unsigned long', as computed by sizeof. */
 #define SIZEOF_UNSIGNED_LONG 8
 
-/* The size of a `unsigned long long', as computed by sizeof. */
+/* The size of `unsigned long long', as computed by sizeof. */
 #define SIZEOF_UNSIGNED_LONG_LONG 8
-
-/* Define to use GDOI. */
-/* #undef SRTP_GDOI */
-
-/* Define to use OpenSSL crypto. */
-/* #undef OPENSSL */
-
-/* Define to compile for kernel contexts. */
-/* #undef SRTP_KERNEL */
-
-/* Define to compile for Linux kernel context. */
-/* #undef SRTP_KERNEL_LINUX */
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Write errors to this file */
-/* #undef USE_ERR_REPORTING_FILE */
-
-/* Define to use syslog logging. */
-/* #undef USE_SYSLOG */
-
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
-/* #undef WORDS_BIGENDIAN */
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
@@ -173,5 +196,5 @@
 /* #undef inline */
 #endif
 
-/* Define to `unsigned' if <sys/types.h> does not define. */
+/* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
